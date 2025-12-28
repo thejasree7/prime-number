709293f082b87b32dc0b7267a76377947f0e4be8
thejasree7 <23701a02c2@aitsrajampet.ac.in>
2025-12-28 11:17:43 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,10 @@
+def primeNumber(n):
+    if n<=1:
+        return "not a prime number"
+    for i in range(2,n):
+        if n%i==0:
+            return "not a prime number"
+    return "prime number"
+    
+n=int(input())
+print(primeNumber(n))
